23b3d43a82b10487b9ad2ce40bfb30c1e76c5862
Shubham Singh <shubhamsingh@Shubhams-Air.bbrouter>
2023-05-05 17:38:16 +0530

    interface

---
+++ untitled/src/Abstraction/inter.java
@@ -0,0 +1,18 @@
+package Abstraction;
+
+public interface inter {
+    int a=10;
+    void m1();
+}
+class C implements inter{
+    public void m1(){
+        System.out.println(10);
+    }
+}
+
+class Main extends C{
+    public static void main(String[] args) {
+        inter r = new C();
+        r.m1();
+    }
+}
