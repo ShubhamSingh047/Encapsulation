eb2a1a8960c7f31d27b3081c3526d55005771d6f
Shubham <shubham.shubham.singh13@gmail.com>
2023-05-10 19:06:58 +0530

    File handleing

---
+++ src/Oops/concepts/PrintInformation.java
@@ -0,0 +1,17 @@
+package Oops.concepts;
+import java.io.*;
+
+public class PrintInformation {
+    public static void main(String[] args) {
+        File f= new File("/Users/shubhamsingh/Developer/Java_OOps/Opps/hello.txt");
+        if(f.exists()){
+            System.out.println("File Name: "+f.getName());
+            System.out.println("File Location: "+f.getAbsolutePath());
+            System.out.println("File Writabale: "+f.canWrite());
+            System.out.println("File Readable: "+f.canRead());
+            System.out.println("File Size: "+f.length());
+        }else{
+            System.out.println("File does not exist");
+        }
+    }
+}
